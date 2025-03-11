96864bcaf0a3fb801beaa63cf0e2d6b1f88a488a
Crystal <crystalreise@student.olympic.edu>
2025-03-11 11:16:13 -0700

    Add files via upload

---
+++ mod8-problems.py.py
@@ -0,0 +1,27 @@
+'''
+#1. Create a file called gardening_tips.txt and add at least 3 gardening tips to it.
+    Write a Python script that reads a file gardening_tips.txt and prints
+    out each tip one by one.
+'''
+
+'''
+#2. Write a Python program that allows users to log their hiking trips. The program should:
+    - Use a while loop to repeatedly ask for a hike name and distance in miles
+    - Save each entry to hiking_log.txt (each hike on a new line)
+    - When the user presses 0, exit the loop & print the contents of hiking_log.txt
+'''
+
+'''
+#3. Create a text file called song_lyrics.txt and copy the lyrics of a song into it. Write a Python program that:
+    - Reads the file
+    - Requests 5 inputs from the user: 5 words the user would like to count the frequency of
+    - Counts how many times each word appears
+    - Creates a dictionary of the words and their counts
+    - Print the dictionary to the console
+'''
+
+'''
+#4. Create a poll.txt file that contains a list of "yea" or "nay" votes separated by commas.
+    Write a program that reads the poll.txt file
+    Count how many votes "yea" or "nay" received and print the results.
+'''
